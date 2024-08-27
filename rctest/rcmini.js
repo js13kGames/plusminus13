94dd4366adf4bd48464304b73ce618ec29ecca9c
Jure Triglav <juretriglav@gmail.com>
2024-08-27 23:27:46 +0200

    Mini en route

--- rctest/rcmini.js
+++ rctest/rcmini.js
@@ -1,5 +1,4 @@
-// This is the html plumbing / structure / controls for little canvases
-function intializeCanvas({ id, canvas, startDrawing }) {
+function intializeCanvas({ id, canvas, render }) {
   const thisId = document.querySelector(`#${id}`);
   thisId.innerHTML = `
     <div style="display: flex; gap: 20px;">
@@ -7,7 +6,7 @@ function intializeCanvas({ id, canvas, startDrawing }) {
   const container = document.querySelector(`#${id}-canvas-container`);
   container.appendChild(canvas);
 
-  canvas.addEventListener("mousedown", startDrawing);
+  render();
 
   return { container };
 }
@@ -19,32 +18,18 @@ void main() {
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }`;
 
-class PaintableCanvas {
-  constructor({ width, height, initialColor = "transparent", radius = 6, friction = 0.1 }) {
-    this.isDrawing = false;
-    this.currentMousePosition = { x: 0, y: 0 };
-    this.lastPoint = { x: 0, y: 0 };
-    this.currentPoint = { x: 0, y: 0 };
-
-    this.currentColor = { r: 255, g: 255, b: 255, a: 255 };
-    this.RADIUS = radius;
-    this.FRICTION = friction;
+class MyCanvas {
+  constructor({ width, height }) {
     this.width = width;
     this.height = height;
-
-    this.initialColor = initialColor;
-
-    this.drawSmoothLine = (from, to) => {
-      throw new Error("Missing implementation");
-    };
   }
 
-  createCanvas(width, height, initialColor) {
+  createCanvas(width, height) {
     const canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
     const context = canvas.getContext("2d");
-    context.fillStyle = initialColor;
+    context.fillStyle = "transparent";
     context.fillRect(0, 0, canvas.width, canvas.height);
     return [canvas, context];
   }
@@ -63,28 +48,8 @@ class PaintableCanvas {
     this.texture.needsUpdate = true;
   }
 
-  startDrawing() {
-    this.isDrawing = true;
-    this.doDraw();
-  }
-
-  stopDrawing() {
-    const wasDrawing = this.isDrawing;
-    if (!wasDrawing) {
-      return false;
-    }
-    this.isDrawing = false;
-    return true;
-  }
-
-  doDraw() {
-    this.drawSmoothLine({ x: 0, y: 0 }, { x: 0, y: 0 });
-    this.lastPoint = this.currentPoint;
-  }
-
   clear() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.currentImageData = new ImageData(this.canvas.width, this.canvas.height);
     this.updateTexture();
   }
 }
@@ -155,7 +120,7 @@ function threeJSInit(
 
 class BaseSurface {
   constructor({ id, width, height, radius = 5, dpr = 1 }) {
-    // Create PaintableCanvas instances
+    // Create MyCanvas instances
     this.createSurface(width, height, radius);
     this.dpr = dpr || window.devicePixelRatio || 1;
     this.width = width;
@@ -166,23 +131,7 @@ class BaseSurface {
   }
 
   createSurface(width, height, radius) {
-    this.surface = new PaintableCanvas({ width, height, radius });
-  }
-
-  initialize() {
-    // Child class should fill this out
-  }
-
-  load() {
-    // Child class should fill this out
-  }
-
-  clear() {
-    // Child class should fill this out
-  }
-
-  renderPass() {
-    // Child class should fill this out
+    this.surface = new MyCanvas({ width, height, radius });
   }
 
   reset() {
@@ -193,8 +142,6 @@ class BaseSurface {
     return intializeCanvas({
       id: this.id,
       canvas: this.canvas,
-      startDrawing: (e) => this.surface.startDrawing(e),
-      stopDrawing: (e, redraw) => this.surface.stopDrawing(e, redraw),
       clear: () => this.clear(),
       reset: () => this.reset(),
       ...this.canvasModifications(),
@@ -223,28 +170,14 @@ class BaseSurface {
   }
 }
 
-class Drawing extends BaseSurface {
+class DistanceField extends BaseSurface {
   initializeSmoothSurface() {
     const props = this.initThreeJS({
       uniforms: {
         inputTexture: { value: this.surface.texture },
-        color: { value: new THREE.Vector4(1, 1, 1, 1) },
-        from: { value: new THREE.Vector2(0, 0) },
-        to: { value: new THREE.Vector2(0, 0) },
-        radiusSquared: { value: Math.pow(this.surface.RADIUS, 2.0) },
-        resolution: { value: new THREE.Vector2(this.width, this.height) },
-        drawing: { value: false },
-        indicator: { value: false },
       },
       fragmentShader: `
 uniform sampler2D inputTexture;
-uniform vec4 color;
-uniform vec2 from;
-uniform vec2 to;
-uniform float radiusSquared;
-uniform vec2 resolution;
-uniform bool drawing;
-uniform bool indicator;
 varying vec2 vUv;
 
 out vec4 FragColor;
@@ -255,27 +188,11 @@ vec4 current = texture(inputTexture, vUv, 0.0);
 FragColor = current;
 }`,
     });
-
-    this.surface.drawSmoothLine = () => {
+    this.surface.render = () => {
       this.triggerDraw();
     };
 
     return props;
-  }
-
-  triggerDraw() {
-    this.renderPass();
-  }
-
-  clear() {
-    if (this.initialized) {
-      this.renderTargets.forEach((target) => {
-        this.renderer.setRenderTarget(target);
-        this.renderer.clearColor();
-      });
-    }
-    this.renderer.setRenderTarget(null);
-    this.renderer.clearColor();
   }
 
   initialize() {
@@ -292,30 +209,7 @@ FragColor = current;
     this.innerInitialize();
   }
 
-  innerInitialize() {}
-
-  load() {
-    this.reset();
-    this.initialized = true;
-  }
-
-  drawPass() {}
-
-  renderPass() {
-    this.drawPass();
-    this.renderer.setRenderTarget(null);
-    this.render();
-  }
-}
-
-class DistanceField extends Drawing {
-  jfaPassesCount() {
-    return this.passes;
-  }
-
   innerInitialize() {
-    super.innerInitialize();
-
     const {
       plane: dfPlane,
       render: dfRender,
@@ -434,50 +328,10 @@ class DistanceField extends Drawing {
   }
 
   renderPass() {
-    let out = this.drawPass();
-    out = this.dfPass(out);
+    let out = this.dfPass(out);
     this.renderer.setRenderTarget(null);
     this.dfRender();
   }
-}
-
-function addSlider({
-  id,
-  name,
-  onUpdate,
-  options = {},
-  hidden = false,
-  initialSpanValue = undefined,
-}) {
-  const div = document.createElement("div");
-  div.style = `display: ${hidden ? "none" : "flex"}; align-items: center; gap: 8px`;
-  document.querySelector(`#${id}`).appendChild(div);
-  div.append(`${name}`);
-  const input = document.createElement("input");
-  input.id = `${id}-${name.replace(" ", "-").toLowerCase()}-slider`;
-  input.className = "slider";
-  input.type = "range";
-  Object.entries(options).forEach(([key, value]) => {
-    input.setAttribute(key, value);
-  });
-  if (options.value) {
-    input.value = options.value;
-  }
-  const span = document.createElement("span");
-  input.setSpan = (value) => (span.innerText = `${value}`);
-
-  input.addEventListener("input", () => {
-    input.setSpan(`${onUpdate(input.value)}`);
-  });
-  span.innerText = `${input.value}`;
-  div.appendChild(input);
-  div.appendChild(span);
-
-  input.onUpdate = onUpdate;
-  if (initialSpanValue != null) {
-    input.setSpan(initialSpanValue);
-  }
-  return input;
 }
 
 class RC extends DistanceField {
@@ -485,40 +339,19 @@ class RC extends DistanceField {
     this.frame = 0;
     this.baseRayCount = 4.0;
     this.reduceDemandCheckbox = document.querySelector("#reduce-demand");
-    this.forceFullPass = !this.reduceDemandCheckbox.checked;
+    this.forceFullPass = true;
     super.innerInitialize();
     this.activelyDrawing = false;
     this.rawBasePixelsBetweenProbes = 1.0;
-
     this.animating = false;
-
-    this.enableSrgb = document.querySelector("#enable-srgb");
-    this.addNoise = document.querySelector("#add-noise");
-    this.ringingFix = document.querySelector("#ringing-fix");
     this.sunAngleSlider = { value: 0 };
 
-    this.rayIntervalSlider = addSlider({
-      id: "radius-slider-container",
-      name: "Interval Length",
-      onUpdate: (value) => {
-        this.rcPlane.material.uniforms.rayInterval.value = value;
-        this.renderPass();
-        return value;
-      },
-      options: { min: 1.0, max: 512.0, step: 1.0, value: 1.0 },
-    });
+    // Uniform parameters
+    const rayInterval = 1.0;
+    const baseRayCount = 1.0; // max 3.0, step 1.0
+    const srgb = 1.0;
 
-    this.baseRayCountSlider = addSlider({
-      id: "radius-slider-container",
-      name: "Base Ray Count",
-      onUpdate: (value) => {
-        this.rcPlane.material.uniforms.baseRayCount.value = Math.pow(4.0, value);
-        this.baseRayCount = Math.pow(4.0, value);
-        this.renderPass();
-        return Math.pow(4.0, value);
-      },
-      options: { min: 1.0, max: 3.0, step: 1.0, value: 1.0 },
-    });
+    this.baseRayCount = Math.pow(4.0, baseRayCount);
 
     this.initializeParameters();
 
@@ -540,75 +373,28 @@ class RC extends DistanceField {
         distanceTexture: { value: null },
         lastTexture: { value: null },
         cascadeExtent: { value: new THREE.Vector2(this.radianceWidth, this.radianceHeight) },
-        cascadeCount: { value: this.radianceCascades },
+        cascadeCount: { value: this.radianceCascades }, // min 1, max this.radianceCascades, step 1
         cascadeIndex: { value: 0.0 },
         basePixelsBetweenProbes: { value: this.basePixelsBetweenProbes },
         cascadeInterval: { value: this.radianceInterval },
-        rayInterval: { value: this.rayIntervalSlider.value },
-        baseRayCount: { value: Math.pow(4.0, this.baseRayCountSlider.value) },
+        rayInterval: { value: rayInterval },
+        baseRayCount: { value: Math.pow(4.0, baseRayCount) },
         sunAngle: { value: this.sunAngleSlider.value },
         time: { value: 0.1 },
-        srgb: { value: this.enableSrgb.checked ? 2.2 : 1.0 },
+        srgb: { value: srgb ? 2.2 : 1.0 },
         enableSun: { value: false },
-        addNoise: { value: this.addNoise.checked },
-        firstCascadeIndex: { value: 0 },
+        addNoise: { value: false },
+        firstCascadeIndex: { value: 0 }, // max = this.radianceCascades - 1, step 1
         time: { value: 0.0 },
       },
       fragmentShader,
     });
 
-    this.baseRayCountSlider.setSpan(Math.pow(4.0, this.baseRayCountSlider.value));
-
     this.firstLayer = this.radianceCascades - 1;
     this.lastLayer = 0;
 
-    this.lastLayerSlider = addSlider({
-      id: "radius-slider-container",
-      name: "(RC) Layer to Render",
-      onUpdate: (value) => {
-        this.rcPlane.material.uniforms.firstCascadeIndex.value = value;
-        this.lastLayer = value;
-        this.renderPass();
-        return value;
-      },
-      options: { min: 0, max: this.radianceCascades - 1, value: 0, step: 1 },
-    });
-
-    this.firstLayerSlider = addSlider({
-      id: "radius-slider-container",
-      name: "(RC) Layer Count",
-      onUpdate: (value) => {
-        this.rcPlane.material.uniforms.cascadeCount.value = value;
-        this.firstLayer = value - 1;
-        this.renderPass();
-        return value;
-      },
-      options: { min: 1, max: this.radianceCascades, value: this.radianceCascades, step: 1 },
-    });
-
-    this.stage = 3;
-    this.stageToRender = addSlider({
-      id: "radius-slider-container",
-      name: "Stage To Render",
-      onUpdate: (value) => {
-        this.stage = value;
-        this.renderPass();
-        return value;
-      },
-      options: { min: 0, max: 3, value: 3, step: 1 },
-    });
-
-    this.pixelsBetweenProbes = addSlider({
-      id: "radius-slider-container",
-      name: "Pixels Between Base Probe",
-      onUpdate: (value) => {
-        this.rawBasePixelsBetweenProbes = Math.pow(2, value);
-        this.initializeParameters(true);
-        this.renderPass();
-        return Math.pow(2, value);
-      },
-      options: { min: 0, max: 4, value: 0, step: 1 },
-    });
+    this.stage = 3; // min 1 max 3 step 1
+    this.rawBasePixelsBetweenProbes = 0.0; // min 0 max 4 step 1
 
     const {
       plane: overlayPlane,
@@ -617,15 +403,13 @@ class RC extends DistanceField {
     } = this.initThreeJS({
       uniforms: {
         inputTexture: { value: null },
-        drawPassTexture: { value: null },
       },
       fragmentShader: `
           uniform sampler2D inputTexture;
-          uniform sampler2D drawPassTexture;
-  
+
           varying vec2 vUv;
           out vec4 FragColor;
-  
+
           void main() {
             vec3 rc = texture(inputTexture, vUv).rgb;
             FragColor = vec4(rc, 1.0);
@@ -670,30 +454,20 @@ class RC extends DistanceField {
   }
 
   overlayPass(inputTexture) {
-    this.overlayPlane.material.uniforms.drawPassTexture.value = this.drawPassTexture;
+    // if (this.forceFullPass) {
+    //   this.frame = 0;
+    // }
 
-    if (this.forceFullPass) {
-      this.frame = 0;
-    }
-
-    if (this.frame == 0 && !this.forceFullPass) {
-      const input = this.overlayRenderTargets[0].texture ?? this.drawPassTexture;
-      this.overlayPlane.material.uniforms.inputTexture.value = input;
-      this.renderer.setRenderTarget(this.overlayRenderTargets[1]);
-      this.overlayRender();
-    } else {
-      this.overlayPlane.material.uniforms.inputTexture.value = inputTexture;
-      this.renderer.setRenderTarget(this.overlayRenderTargets[0]);
-      this.overlayRender();
-    }
+    // this.overlayPlane.material.uniforms.inputTexture.value = inputTexture;
+    // this.renderer.setRenderTarget(this.overlayRenderTargets[0]);
+    // this.overlayRender();
 
     if (!this.isDrawing) {
       this.overlay = true;
-      const frame = this.forceFullPass ? 0 : 1 - this.frame;
-      this.plane.material.uniforms.inputTexture.value = this.overlayRenderTargets[frame].texture;
-      this.plane.material.uniforms.indicator.value = true;
-      this.surface.drawSmoothLine(this.surface.currentPoint, this.surface.currentPoint);
-      this.plane.material.uniforms.indicator.value = false;
+      const frame = 0; // this.forceFullPass ? 0 : 1 - this.frame;
+      this.plane.material.uniforms.inputTexture.value = inputTexture;
+
+      this.triggerDraw();
       this.overlay = false;
     }
   }
@@ -704,23 +478,19 @@ class RC extends DistanceField {
       this.render();
       return;
     }
-    super.triggerDraw();
+    this.renderPass();
   }
 
   canvasModifications() {
     return {
-      startDrawing: (e) => {
-        this.surface.startDrawing(e);
-      },
-      stopDrawing: (e, redraw) => {
-        this.surface.stopDrawing(e, redraw);
+      render: (e) => {
+        this.triggerDraw();
       },
     };
   }
 
-  rcPass(distanceFieldTexture, drawPassTexture) {
+  rcPass(distanceFieldTexture) {
     this.rcPlane.material.uniforms.distanceTexture.value = distanceFieldTexture;
-    this.rcPlane.material.uniforms.sceneTexture.value = drawPassTexture;
 
     if (this.frame == 0) {
       this.rcPlane.material.uniforms.lastTexture.value = null;
@@ -736,6 +506,7 @@ class RC extends DistanceField {
       this.renderer.setRenderTarget(this.rcRenderTargets[this.prev]);
       this.rcRender();
       this.rcPlane.material.uniforms.lastTexture.value = this.rcRenderTargets[this.prev].texture;
+
       this.prev = 1 - this.prev;
     }
 
@@ -749,41 +520,28 @@ class RC extends DistanceField {
       if (this.stage == 0) {
         this.renderer.setRenderTarget(null);
         this.render();
-        this.finishRenderPass();
         return;
       }
 
       if (this.stage == 1) {
-        this.finishRenderPass();
         this.renderer.setRenderTarget(null);
         return;
       }
 
-      this.distanceFieldTexture = this.dfPass(this.drawPassTexture);
+      this.distanceFieldTexture = this.dfPass();
 
       if (this.stage == 2) {
-        this.finishRenderPass();
         this.renderer.setRenderTarget(null);
         this.dfRender();
         return;
       }
     }
 
-    let rcTexture = this.rcPass(this.distanceFieldTexture, this.drawPassTexture);
-
+    const rcTexture = this.rcPass(this.distanceFieldTexture);
     this.overlayPass(rcTexture);
-    this.finishRenderPass();
   }
 
-  finishRenderPass() {
-    if (!this.forceFullPass) {
-      this.frame = 1 - this.frame;
-    }
-  }
-
-  // foo bar baz!!
   renderPass() {
-    this.drawPassTexture = this.drawPass();
     if (!this.animating) {
       this.animating = true;
       requestAnimationFrame(() => {
@@ -815,24 +573,7 @@ class RC extends DistanceField {
     this.renderPass();
   }
 
-  //foo bar baz!!
   load() {
-    this.reduceDemandCheckbox.addEventListener("input", () => {
-      this.forceFullPass = !this.reduceDemandCheckbox.checked;
-      this.renderPass();
-    });
-    this.enableSrgb.addEventListener("input", () => {
-      this.rcPlane.material.uniforms.srgb.value = this.enableSrgb.checked ? 2.2 : 1.0;
-      this.renderPass();
-    });
-    this.addNoise.addEventListener("input", () => {
-      this.rcPlane.material.uniforms.addNoise.value = this.addNoise.checked;
-      this.renderPass();
-    });
-    this.sunAngleSlider.addEventListener("input", () => {
-      this.rcPlane.material.uniforms.sunAngle.value = this.sunAngleSlider.value;
-      this.renderPass();
-    });
     this.reset();
     this.initialized = true;
   }
@@ -841,11 +582,6 @@ class RC extends DistanceField {
     this.clear();
   }
 }
-// Get all query parameters
-const urlParams = new URLSearchParams(window.location.search);
-
-// Get a specific parameter
 
 let [width, height] = [window.innerWidth, window.innerHeight];
-
 new RC({ id: "radiance-cascades-canvas", width, height, radius: 4 });
